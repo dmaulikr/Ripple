6873e3bc7e0123240017f7430893ca847e28665d
keithlee <keithlee002@gmail.com>
2016-12-19 18:52:10 -0800

    Initial prototype

---
+++ Ripple/UIColorExtensions.swift
@@ -0,0 +1,16 @@
+//
+//  UIColorExtensions.swift
+//  Ripple
+//
+//  Created by Keith Lee on 12/19/16.
+//  Copyright © 2016 Keith Lee. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static let rippleGreen = UIColor(red: 107/255, green: 215/255, blue: 164/255, alpha: 1)
+    static let rippleGrey = UIColor(red: 245/255, green: 245/255, blue: 245/255, alpha: 1)
+    static let rippleBeige = UIColor(red: 239/255, green: 237/255, blue: 231/255, alpha: 1)
+}
--- Ripple/ViewController.swift
+++ Ripple/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var circleView = UIView(frame: CGRect.zero)
+    var dotView = UIView(frame: CGRect.zero)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.rippleGrey
+        
+        let size: CGFloat = 40
+        circleView.frame = CGRect(x: view.bounds.width/2 - size/2, y: view.bounds.height/2 - size/2, width: size, height: size)
+        circleView.layer.cornerRadius = size/2
+        circleView.layer.borderColor = UIColor.rippleGreen.cgColor
+        circleView.layer.borderWidth = 1
+        
+        let dotSize: CGFloat = 4
+        dotView.frame = CGRect(x: view.bounds.width/2 - dotSize/2, y: view.bounds.height/2 - dotSize/2, width: dotSize, height: dotSize)
+        dotView.layer.cornerRadius = dotSize/2
+        dotView.backgroundColor = UIColor.rippleGreen
+        dotView.alpha = 0.7
+        view.addSubview(circleView)
+        start()
+   }
+    
+    func start() {
+        UIView.animate(withDuration: 5, delay: 0, options: [], animations: {
+            self.circleView.transform = self.circleView.transform.scaledBy(x: 5, y: 5)
+        }, completion: { _ in
+            self.view.addSubview(self.dotView)
+            self.shrinkCircle()
+        })
     }
-
+    
+    func shrinkCircle() {
+        Timer.scheduledTimer(withTimeInterval: 1.5, repeats: false, block: { _ in
+            self.dotView.removeFromSuperview()
+        })
+        UIView.animate(withDuration: 5, delay: 1.5, options: [], animations: {
+            self.circleView.transform = self.circleView.transform.scaledBy(x: 1/5, y: 1/5)
+        }, completion: { _ in
+            Timer.scheduledTimer(withTimeInterval: 1, repeats: false, block: { (timer) in
+                self.start()
+            })
+        })
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
