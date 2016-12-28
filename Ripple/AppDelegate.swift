49ac632867cb1595be327e5aadb72427cb87c23d
keithlee <keithlee002@gmail.com>
2016-12-28 00:20:05 -0800

    Dark theme default again

--- Ripple/AppDelegate.swift
+++ Ripple/AppDelegate.swift
@@ -23,6 +23,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         Fabric.with([Crashlytics.self])
+        UserDefaults.ripple?.register(defaults: [nightThemeKey: true])
         UIApplication.shared.isIdleTimerDisabled = true
         return true
     }
